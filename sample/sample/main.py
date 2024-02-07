c3828e7b55b5d6a7efedabf761174e87064d564b
Ganesh Tiwari <ganesht049@gmail.com>
2024-02-07 15:26:30 +0530

    feat: added secrets action

--- sample/sample/main.py
+++ sample/sample/main.py
@@ -9,3 +9,7 @@ def main():
   message = os.getenv("MESSAGE")
   
   print(f"This is the message: {message}")
+  
+  secret_persons_name = os.getenv("SECRET_PERSONS_NAME")
+  
+  print(f"Aapka Naam: {secret_persons_name}")
