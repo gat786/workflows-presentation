aa3db7fe932c40b260eeca8b9a623978458e3717
Ganesh Tiwari <ganesht049@gmail.com>
2024-02-07 15:17:02 +0530

    feat: added env variable

--- sample/sample/main.py
+++ sample/sample/main.py
@@ -1,2 +1,11 @@
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 def main():
   print("Hello, World!")
+  
+  message = os.getenv("MESSAGE")
+  
+  print(f"This is the message: {message}")
